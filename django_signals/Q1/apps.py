a50875485e9dabb9429dea00b526ed42f99098da
Badal Nagpal <77916085+BadalNagpal@users.noreply.github.com>
2024-09-29 12:02:50 +0530

    deleted

--- django_signals/Q1/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MyAppConfig(AppConfig):
-    name = 'app'
-
-    def ready(self):
-        #importing the signals module to register the signal handlers
-        from . import signals
